c0707ed59faf8e5a3e7f8d78a91c01e84faf1838
kirank22 <45580946+kirank22@users.noreply.github.com>
2025-03-23 15:36:51 -0400

    setup

---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Kiran Kothapalli on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LOTRConverter/LOTRConverterTests/LOTRConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterTests.swift
+//  LOTRConverterTests
+//
+//  Created by Kiran Kothapalli on 3/23/25.
+//
+
+import Testing
+@testable import LOTRConverter
+
+struct LOTRConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
